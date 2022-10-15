ad5a50fabdfc779c1c5421a7be5ef3d3c34914c9
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-15 12:09:44 +0100

    read file async

--- src/fs-module.js
+++ src/fs-module.js
@@ -1,4 +1,4 @@
-const {readFileSync, writeFileSync} = require('fs')
+const {readFileSync, writeFileSync, readFile, writeFile} = require('fs')
 
 const filedata = readFileSync('./content/fs.txt', 'utf8');
 
@@ -8,3 +8,14 @@ writeFileSync(
     './content/new-file.txt',
     `this what was read before: ${filedata}`
 );
+
+// read, write async
+
+readFile('./content/fs.txt', (err, fileData) => {
+    if (err)
+    {
+        console.log(err);
+        return ;
+    }
+    console.log(`data is ready: ${fileData}`);
+});
