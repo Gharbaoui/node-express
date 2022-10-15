267037d6ae7fb00e2ae19c0a2a2055d6d9284347
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-15 11:54:27 +0100

    how to write to file if it does not exist it will create one and it exist it will overwrite sync

--- src/fs-module.js
+++ src/fs-module.js
@@ -3,3 +3,8 @@ const {readFileSync, writeFileSync} = require('fs')
 const filedata = readFileSync('./content/fs.txt', 'utf8');
 
 console.log(filedata);
+
+writeFileSync(
+    './content/new-file.txt',
+    `this what was read before: ${filedata}`
+);
