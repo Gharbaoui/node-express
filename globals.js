f295d6bbc64678bf19f9e052bff19cd455c9a9db
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-15 10:57:00 +0100

    globals

---
+++ globals.js
@@ -0,0 +1,8 @@
+// globals are some variables or function that are available to all files no matter how much nested are
+/*
+    for example:
+        __dirname is variable that has info about current directory
+        require is function to load other modules
+*/
+
+console.log(__dirname);
