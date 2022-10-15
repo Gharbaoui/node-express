176b4bf3f512f908fa99c1a10649bc1394df9485
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-15 11:38:03 +0100

    get info about the current os

--- src/builtin-modules.js
+++ src/builtin-modules.js
@@ -3,3 +3,12 @@ const os = require('os');
 const user = os.userInfo();
 
 console.log(user);
+
+const   infoOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+};
+
+console.log(infoOS);
