a3503e4ce61069fa77221257f16d88c1fb5934a5
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-17 18:04:44 +0100

    examples of query

--- src/node-express/app.js
+++ src/node-express/app.js
@@ -20,6 +20,32 @@ app.get('/api/product/:productID', (req, res) => {
     res.json(pro);
 });
 
+app.get('/api/v1/pages/:pageID', (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+});
+
+app.get('/api/v1/query', (req, res) => {
+    const {search, limit} = req.query;
+    let productsThatMatch = [];
+    const arr_len = products.length;
+    let max = arr_len;
+    let counter = 0;
+    if (limit)
+        max = Number(limit);
+
+    if (search)
+    {
+        for (let i = 0; i < arr_len && counter < max; ++i) {
+            if (products[i].name.startsWith(search)){
+                productsThatMatch.push(products[i]);
+                ++counter;
+            }
+        }
+    }
+    res.json(productsThatMatch);
+});
+
 app.listen(2000, () => {
     console.log(`listning on port 2000....`);
 });
