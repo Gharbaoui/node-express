33793adbf1145303ce31c25cf367c6e861e5c068
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-17 19:45:56 +0100

    some logic in authorize that's actually liegit add authorize.js. and i noticed that the app is getting bigger, but what is the solution? use express router so we can group routs togther

--- src/node-express/authorize.js
+++ src/node-express/authorize.js
@@ -1,6 +1,14 @@
 const authorize = (req, res, next) => {
-    console.log(`authorized`);
-    next();
+    console.log(`checking....`);
+    const {user} = req.query;
+    if (user === 'mohamed')
+    {
+        req.user = {user: user, id: 4}
+        next();
+    } else {
+        res.status(401).send(`you are not allowed here!!`);
+    }
+    console.log(`middelware end`);
 };
 
 module.exports = {authorize};
